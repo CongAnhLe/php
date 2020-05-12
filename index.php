b3e54cd442fb1f0ec892fcd316ca3c4a821dba3e
congla <congla@ghn.vn>
2020-05-12 11:12:56 +0700

    update index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $value = "izz";
 setcookie($name, $value, [
     'expires' => time() + 86400,
     'path' => '/',
-    'domain' => 'php-demo-01.herokuapp.com',
+    'domain' => 'boiling-beyond-67908.herokuapp.com',
     'samesite' => 'Strict',
 ]);
 
